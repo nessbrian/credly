65e622a251368f71bf485cce85f5a464398a9c22
Brian <bcn@bubblecreek.com>
2019-10-29 12:14:41 -0500

    Refactor 'Badge' model.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,13 +1,26 @@
 class Badge < ApplicationRecord
+  # TODO: protect this by moving it to an ENV variable.
+  @@auth = {:username => "Bw_yergO8Gl-K3A8e-1I2fBQQCnTu6pQcUUP:", :password => ""}
+
+  base_url = 'https://sandbox-api.youracclaim.com/v1/organizations/'
+  organiztion_id = '91ebc580-e14b-4483-9456-7bcc8db79e68/'
+  @@url = base_url + organiztion_id + 'badges'
 
   def self.all
-    base_url = 'https://sandbox-api.youracclaim.com/v1/organizations/'
-    organiztion_id = '91ebc580-e14b-4483-9456-7bcc8db79e68/'
-    url = base_url + organiztion_id + 'badges'
-    auth = {:username => "Bw_yergO8Gl-K3A8e-1I2fBQQCnTu6pQcUUP:", :password => ""}
-
-    response = HTTParty.get(url, {:basic_auth => auth})
+    response = HTTParty.get(@@url, :basic_auth => @@auth)
     response.parsed_response["data"]
   end
 
+  def self.new(badge_params)
+    data = {
+      "recipient_email": badge_params['recipient_email'],
+      "badge_template_id": badge_params['badge_template_id'],
+      "issued_at": Time.now,
+      "issued_to_first_name": "John",
+      "issued_to_last_name": "Doe"
+    }
+    response = HTTParty.post(@@url, :body => data, :basic_auth => @@auth)
+    #response.parsed_response["data"]
+  end
 end
+
