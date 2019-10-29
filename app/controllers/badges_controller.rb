cfcd6ce9d41183fea621e38fd06ffced592705cb
Brian <bcn@bubblecreek.com>
2019-10-29 09:36:32 -0500

    Clean up BadgesController.

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,28 +1,20 @@
 class BadgesController < ApplicationController
   before_action :set_badge, only: [:show, :edit, :update, :destroy]
 
-  # GET /badges
-  # GET /badges.json
   def index
     @badges = Badge.all
   end
 
-  # GET /badges/1
-  # GET /badges/1.json
   def show
   end
 
-  # GET /badges/new
   def new
     @badge = Badge.new
   end
 
-  # GET /badges/1/edit
   def edit
   end
 
-  # POST /badges
-  # POST /badges.json
   def create
     @badge = Badge.new(badge_params)
 
@@ -34,30 +26,6 @@ class BadgesController < ApplicationController
         format.html { render :new }
         format.json { render json: @badge.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /badges/1
-  # PATCH/PUT /badges/1.json
-  def update
-    respond_to do |format|
-      if @badge.update(badge_params)
-        format.html { redirect_to @badge, notice: 'Badge was successfully updated.' }
-        format.json { render :show, status: :ok, location: @badge }
-      else
-        format.html { render :edit }
-        format.json { render json: @badge.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /badges/1
-  # DELETE /badges/1.json
-  def destroy
-    @badge.destroy
-    respond_to do |format|
-      format.html { redirect_to badges_url, notice: 'Badge was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
