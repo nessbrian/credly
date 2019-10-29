66cc4fee5f230f8b1d451b25a322b94e46cdc79a
Brian <bcn@bubblecreek.com>
2019-10-29 09:39:04 -0500

    Define class method 'all' in Badge model.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,3 +1,13 @@
 class Badge < ApplicationRecord
-	
+
+  def self.all
+    base_url = 'https://sandbox-api.youracclaim.com/v1/organizations/'
+    organiztion_id = '91ebc580-e14b-4483-9456-7bcc8db79e68/'
+    url = base_url + organiztion_id + 'badges'
+    auth = {:username => "Bw_yergO8Gl-K3A8e-1I2fBQQCnTu6pQcUUP:", :password => ""}
+
+    response = HTTParty.get(url, {:basic_auth => auth})
+    response.parsed_response["data"]
+  end
+
 end
